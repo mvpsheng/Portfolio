361a1bb42a50c198ff390d384578a7f2ddfbf8b4
mvpsheng <51054870+mvpsheng@users.noreply.github.com>
2023-06-28 05:13:07 +0000

    Initial commit

---
+++ src/components/Project.jsx
@@ -0,0 +1,8 @@
+export default function Project() {
+  return (
+    <div>
+      <div>Project</div>
+      <div>Contents</div>
+    </div>
+  )
+}
---
+++ src/components/SayHello.jsx
@@ -0,0 +1,8 @@
+export default function SayHello() {
+  return (
+    <div>
+      <div>SayHello</div>
+      <div>Contents</div>
+    </div>
+  )
+}
